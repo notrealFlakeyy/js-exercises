a0b1a639fbb5812bd1f31c25cff9f91ae5c56a0c
notrealFlakeyy <svenslucas@icloud.com>
2023-04-28 13:49:27 +0300

    added changes

--- calculate-age/app.js
+++ calculate-age/app.js
@@ -1,18 +1,35 @@
-function calculateAge() {
-    const birthDay = document.getElementById("day").value;
-    const birthMonth = document.getElementById("month").value;
-    const birthYear = document.getElementById("year").value;
+const inputElement = document.querySelector('input');
+const divElement = document.querySelector('div');
 
-    const today = new Date();
-    const currentYear = today.getFullYear();
-    const currentMonth = today.getMonth() + 1;
-    const currentDay = today.getDate();
+inputElement.addEventListener('change', () => {
+    const result = computeAge(inputElement.value);
+    divElement.innerHTML = `
+    Du är ${result.years} år och ${result.days} dagar gammal
+  `;
+});
 
-    let age = currentYear - birthYear;
+/**
+ * @param {string} dateOfBirth 
+ * @returns {{years: number, days: number}}
+ */
+function computeAge(dateOfBirth) {
+    const now = new Date();
+    const birtdate = new Date(dateOfBirth);
 
-    if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDay < birthDay)) {
-        age--;
+    if ((now.getMonth() > birtdate.getMonth()) || (now.getMonth() === birtdate.getMonth() && now.getDate() >= birtdate.getDate())) {
+        const thisYearBirtDate = new Date(`${now.getFullYear()}-${birtdate.getMonth() + 1}-${birtdate.getDate()}`);
+
+        return {
+            years: now.getFullYear() - birtdate.getFullYear(),
+            days: Math.floor((now.getTime() - thisYearBirtDate.getTime()) / (1000 * 60 * 60 * 24))
+        };
     }
+    else {
+        const lastYearBirtDate = new Date(`${now.getFullYear() - 1}-${birtdate.getMonth() + 1}-${birtdate.getDate()}`);
 
-    document.getElementById("age").textContent = age;
+        return {
+            years: now.getFullYear() - birtdate.getFullYear() - 1,
+            days: Math.floor((now.getTime() - lastYearBirtDate.getTime()) / (1000 * 60 * 60 * 24))
+        }
+    }
 }
